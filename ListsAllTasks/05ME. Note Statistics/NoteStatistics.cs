f1b9b08b0a5a5a3376d204518878bebef2a56b39
VenciSarov <venci.sarov@gmail.com>
2017-02-21 13:13:56 +0200

    Tasks (05ME Note statistic & 06E Stuck Zipper) was solved!

--- ListsAllTasks/05ME. Note Statistics/NoteStatistics.cs
+++ ListsAllTasks/05ME. Note Statistics/NoteStatistics.cs
@@ -1,15 +1,120 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _05ME.Note_Statistics
+﻿namespace _05ME.Note_Statistics
 {
-    class Program
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class NoteStatistics
     {
-        static void Main(string[] args)
+        public static void Main()
         {
+            var inputFrequencieses = Console.ReadLine().Split(' ').Select(double.Parse).ToList();
+
+            var frequenciesNotes = new Dictionary<double, string>();
+            frequenciesNotes[261.63] = "C";
+            frequenciesNotes[277.18] = "C#";
+            frequenciesNotes[293.66] = "D";
+            frequenciesNotes[311.13] = "D#";
+            frequenciesNotes[329.63] = "E";
+            frequenciesNotes[349.23] = "F";
+            frequenciesNotes[369.99] = "F#";
+            frequenciesNotes[392.00] = "G";
+            frequenciesNotes[415.30] = "G#";
+            frequenciesNotes[440.00] = "A";
+            frequenciesNotes[466.16] = "A#";
+            frequenciesNotes[493.88] = "B";
+
+            var notes = GetNotes(inputFrequencieses, frequenciesNotes);
+            var natural = GetNaturals(inputFrequencieses, frequenciesNotes);
+            var sharps = GetSharps(inputFrequencieses, frequenciesNotes);
+            double naturalsSum = GetNaturalSum(natural, frequenciesNotes);
+            double sharpsSum = GetSharpsSum(sharps, frequenciesNotes);
+
+            Console.WriteLine($"Notes: {string.Join(" ", notes)}");
+            Console.WriteLine($"Naturals: {string.Join(", ", natural)}");
+            Console.WriteLine($"Sharps: {string.Join(", ", sharps)}");
+            Console.WriteLine("Naturals sum: {0}", naturalsSum);
+            Console.WriteLine("Sharps sum: {0}", sharpsSum);
+        }
+
+        static double GetSharpsSum(List<string> sharps, Dictionary<double, string> frequenciesNotes)
+        {
+            double sharpsSum = 0;
+            var reversedDict = frequenciesNotes.ToDictionary(kvp => kvp.Value, kvp => kvp.Key);
+
+            foreach (var item in sharps)
+            {
+                if (reversedDict.ContainsKey(item))
+                {
+                    sharpsSum += reversedDict[item];
+                }
+            }
+
+            return sharpsSum;
+        }
+
+        static double GetNaturalSum(List<string> natural, Dictionary<double, string> frequenciesNotes)
+        {
+            double naturalSum = 0;
+            var reversedDict = frequenciesNotes.ToDictionary(kvp => kvp.Value, kvp => kvp.Key);
+
+            foreach (var item in natural)
+            {
+                if (reversedDict.ContainsKey(item))
+                {
+                    naturalSum += reversedDict[item];
+                }
+            }
+
+            return naturalSum;
+        }
+
+        static List<string> GetNaturals(List<double> inputFrequencieses, Dictionary<double, string> frequenciesNotes)
+        {
+            var notes = GetNotes(inputFrequencieses, frequenciesNotes);
+            var naturalNotes = new List<string>();
+
+            foreach (var item in notes)
+            {
+                if (!item.Contains("#"))
+                {
+                    naturalNotes.Add(item);
+                }
+            }
+
+            return naturalNotes;
+        }
+
+        private static List<string> GetSharps(List<double> inputFrequencieses, Dictionary<double, string> frequenciesNotes)
+        {
+            var existingNotes = GetNotes(inputFrequencieses, frequenciesNotes);
+            var natural = GetNaturals(inputFrequencieses, frequenciesNotes);
+            var sharps = new List<string>();
+
+            foreach (var item in existingNotes)
+            {
+                if (!natural.Contains(item))
+                {
+                    sharps.Add(item);
+                }
+            }
+
+            return sharps;
+        }
+
+        static List<string> GetNotes(List<double> inputFrequencieses, Dictionary<double, string> frequenciesNotes)
+        {
+            var existingNotes = new List<string>();
+
+            foreach (var item in inputFrequencieses)
+            {
+                if (frequenciesNotes.ContainsKey(item))
+                {
+                    existingNotes.Add(frequenciesNotes[item]);
+                }
+            }
+
+            return existingNotes;
         }
     }
 }
--- ListsAllTasks/06E. Stuck Zipper/StuckZipper.cs
+++ ListsAllTasks/06E. Stuck Zipper/StuckZipper.cs
@@ -10,18 +10,117 @@
         {
             var firstListNumbers = Console.ReadLine().Split(' ').ToList();
             var secondListNumbers = Console.ReadLine().Split(' ').ToList();
-            int minLenghtOfDigits = int.MaxValue;
+            int minLengthOfDigits = int.MaxValue;
 
-            foreach (var num in firstListNumbers)
+            minLengthOfDigits = GetMinLengthOfEachDigit(firstListNumbers, secondListNumbers, minLengthOfDigits);
+            RemovingLargerLengthDigits(firstListNumbers, secondListNumbers, minLengthOfDigits);
+            ZipAndPrintLists(firstListNumbers, secondListNumbers);
+        }
+
+        static void ZipAndPrintLists(List<string> firstListNumbers, List<string> secondListNumbers)
+        {
+            var resultList = new List<string>();
+            int max = Math.Max(firstListNumbers.Count, secondListNumbers.Count);
+
+            for (int i = 0; i < max; i++)
             {
-                int numLenght = num.Length;
-
-                if (numLenght < minLenghtOfDigits)
+                if (i < secondListNumbers.Count)
                 {
-                    minLenghtOfDigits = numLenght;
+                    resultList.Add(secondListNumbers[i]);
+                }
+                if (i < firstListNumbers.Count)
+                {
+                    for (int r = 0; r < 1; r++)
+                    {
+                        resultList.Add(firstListNumbers[i]);
+                    }
                 }
             }
 
+            Console.WriteLine(string.Join(" ", resultList));
+        }
+
+        static void RemovingLargerLengthDigits(List<string> firstListNumbers, List<string> secondListNumbers, int minLenghtOfDigits)
+        {
+            for (int i = 0; i < firstListNumbers.Count; i++)
+            {
+                if (!firstListNumbers[i].Contains("-"))
+                {
+                    if (firstListNumbers[i].Length > minLenghtOfDigits)
+                    {
+                        firstListNumbers.Remove(firstListNumbers[i]);
+                        i--;
+                    }
+                }
+                else
+                {
+                    if (firstListNumbers[i].Length - 1 > minLenghtOfDigits)
+                    {
+                        firstListNumbers.Remove(firstListNumbers[i]);
+                        i--;
+                    }
+                }
+            }
+            for (int i = 0; i < secondListNumbers.Count; i++)
+            {
+                if (!secondListNumbers[i].Contains("-"))
+                {
+                    if (secondListNumbers[i].Length > minLenghtOfDigits)
+                    {
+                        secondListNumbers.Remove(secondListNumbers[i]);
+                        i--;
+                    }
+                }
+                else
+                {
+                    if (secondListNumbers[i].Length - 1 > minLenghtOfDigits)
+                    {
+                        secondListNumbers.Remove(secondListNumbers[i]);
+                        i--;
+                    }
+                }
+            }
+        }
+
+        static int GetMinLengthOfEachDigit(List<string> firstListNumbers, List<string> secondListNumbers, int minLenghtOfDigits)
+        {
+            foreach (var num in firstListNumbers)
+            {
+                if (!num.Contains("-"))
+                {
+                    if (num.Length < minLenghtOfDigits)
+                    {
+                        minLenghtOfDigits = num.Length;
+                    }
+                }
+                else
+                {
+                    if (num.Length - 1 < minLenghtOfDigits)
+                    {
+                        minLenghtOfDigits = num.Length - 1;
+                    }
+                }
+            }
+
+            foreach (var num in secondListNumbers)
+            {
+                if (!num.Contains("-"))
+                {
+                    if (num.Length < minLenghtOfDigits)
+                    {
+                        minLenghtOfDigits = num.Length;
+                    }
+                }
+                else
+                {
+                    if (num.Length - 1 < minLenghtOfDigits)
+                    {
+                        minLenghtOfDigits = num.Length - 1;
+                    }
+                }
+            }
+
+            return minLenghtOfDigits;
         }
     }
 }
